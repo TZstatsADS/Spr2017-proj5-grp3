75a1a2789194f623a803b09f8ec81466932e8be1
Chenyun Zhu <cz2434@columbia.edu>
2017-04-15 23:34:33 -0400

    update

--- lib/feature_extraction.R
+++ lib/feature_extraction.R
@@ -2,6 +2,7 @@
 #        is the one you want to extract corresponding features
 #        from the background.csv
 # Output: a data frame.
+
 feature <- function(B = background, df){
   index <- colnames(B) %in% df$Code
   output <- B[,index] 
@@ -13,8 +14,8 @@ feature <- function(B = background, df){
 # setwd("~/Desktop/GR5243/Spr2017-proj5-grp3")
 # background <- read.csv("data/FFChallenge/background.csv")
 # dad <- read.csv("data/ff_dad_cb9.csv")
-
-
-###########Sample#################
-# test <- feature(background, dad)
+# 
+# 
+# ###########Sample#################
+# lets_test <- feature(background, dad)
 
